0e6d131919dafab80084376f2153501634c116fc
smc5720 <smc5720@gmail.com>
2019-10-15 22:16:04 +0900

    20191015 SEO

--- DataParse(SEO)/app/src/main/java/kau/smc/dataparseseo/MainActivity.java
+++ DataParse(SEO)/app/src/main/java/kau/smc/dataparseseo/MainActivity.java
@@ -43,45 +43,44 @@ public class MainActivity extends Activity {
             parser.setInput(url.openStream(), null);
 
             int parserEvent = parser.getEventType();
-            System.out.println("파싱시작합니다.");
 
             while (parserEvent != XmlPullParser.END_DOCUMENT) {
                 switch (parserEvent) {
                     case XmlPullParser.START_TAG://parser가 시작 태그를 만나면 실행
-                        if (parser.getName().equals("buildAddress")) { //title 만나면 내용을 받을수 있게 하자
+                        if (parser.getName().equals("buildAddress")) { //buildAddress 만나면 내용을 받을수 있게 하자
                             inbuildAddress = true;
                         }
-                        if (parser.getName().equals("buildPlace")) { //address 만나면 내용을 받을수 있게 하자
+                        if (parser.getName().equals("buildPlace")) { //buildPlace 만나면 내용을 받을수 있게 하자
                             inbuildPlace = true;
                         }
-                        if (parser.getName().equals("manager")) { //mapx 만나면 내용을 받을수 있게 하자
+                        if (parser.getName().equals("manager")) { //manager 만나면 내용을 받을수 있게 하자
                             inmanager = true;
                         }
-                        if (parser.getName().equals("managerTel")) { //mapy 만나면 내용을 받을수 있게 하자
+                        if (parser.getName().equals("managerTel")) { //managerTel 만나면 내용을 받을수 있게 하자
                             inmanagerTel = true;
                         }
-                        if (parser.getName().equals("mfg")) { //mapy 만나면 내용을 받을수 있게 하자
+                        if (parser.getName().equals("mfg")) { //mfg 만나면 내용을 받을수 있게 하자
                             inmfg = true;
                         }
-                        if (parser.getName().equals("model")) { //mapy 만나면 내용을 받을수 있게 하자
+                        if (parser.getName().equals("model")) { //model 만나면 내용을 받을수 있게 하자
                             inmodel = true;
                         }
-                        if (parser.getName().equals("org")) { //mapy 만나면 내용을 받을수 있게 하자
+                        if (parser.getName().equals("org")) { //org 만나면 내용을 받을수 있게 하자
                             inorg = true;
                         }
-                        if (parser.getName().equals("rnum")) { //mapy 만나면 내용을 받을수 있게 하자
+                        if (parser.getName().equals("rnum")) { //rnum 만나면 내용을 받을수 있게 하자
                             inrnum = true;
                         }
-                        if (parser.getName().equals("wgs84Lat")) { //mapy 만나면 내용을 받을수 있게 하자
+                        if (parser.getName().equals("wgs84Lat")) { //wgs84Lat 만나면 내용을 받을수 있게 하자
                             inwgs84Lat = true;
                         }
-                        if (parser.getName().equals("wgs84Lon")) { //mapy 만나면 내용을 받을수 있게 하자
+                        if (parser.getName().equals("wgs84Lon")) { //wgs84Lon 만나면 내용을 받을수 있게 하자
                             inwgs84Lon = true;
                         }
-                        if (parser.getName().equals("zipcode1")) { //mapy 만나면 내용을 받을수 있게 하자
+                        if (parser.getName().equals("zipcode1")) { //zipcode1 만나면 내용을 받을수 있게 하자
                             inzipcode1 = true;
                         }
-                        if (parser.getName().equals("zipcode2")) { //mapy 만나면 내용을 받을수 있게 하자
+                        if (parser.getName().equals("zipcode2")) { //zipcode2 만나면 내용을 받을수 있게 하자
                             inzipcode2 = true;
                         }
                         if (parser.getName().equals("message")) { //message 태그를 만나면 에러 출력
@@ -91,51 +90,51 @@ public class MainActivity extends Activity {
                         break;
 
                     case XmlPullParser.TEXT://parser가 내용에 접근했을때
-                        if (inbuildAddress) { //isTitle이 true일 때 태그의 내용을 저장.
+                        if (inbuildAddress) { //inbuildAddress true일 때 태그의 내용을 저장.
                             buildAddress = parser.getText();
                             inbuildAddress = false;
                         }
-                        if (inbuildPlace) { //isAddress이 true일 때 태그의 내용을 저장.
+                        if (inbuildPlace) { //inbuildPlace true일 때 태그의 내용을 저장.
                             buildPlace = parser.getText();
                             inbuildPlace = false;
                         }
-                        if (inmanager) { //isMapx이 true일 때 태그의 내용을 저장.
+                        if (inmanager) { //inmanager true일 때 태그의 내용을 저장.
                             manager = parser.getText();
                             inmanager = false;
                         }
-                        if (inmanagerTel) { //isMapy이 true일 때 태그의 내용을 저장.
+                        if (inmanagerTel) { //inmanagerTel true일 때 태그의 내용을 저장.
                             managerTel = parser.getText();
                             inmanagerTel = false;
                         }
-                        if (inmfg) { //isMapy이 true일 때 태그의 내용을 저장.
+                        if (inmfg) { //inmfg true일 때 태그의 내용을 저장.
                             mfg = parser.getText();
                             inmfg = false;
                         }
-                        if (inmodel) { //isMapy이 true일 때 태그의 내용을 저장.
+                        if (inmodel) { //inmodel true일 때 태그의 내용을 저장.
                             model = parser.getText();
                             inmodel = false;
                         }
-                        if (inorg) { //isMapy이 true일 때 태그의 내용을 저장.
+                        if (inorg) { //inorg true일 때 태그의 내용을 저장.
                             org = parser.getText();
                             inorg = false;
                         }
-                        if (inrnum) { //isMapy이 true일 때 태그의 내용을 저장.
+                        if (inrnum) { //inrnum true일 때 태그의 내용을 저장.
                             rnum = parser.getText();
                             inrnum = false;
                         }
-                        if (inwgs84Lat) { //isMapy이 true일 때 태그의 내용을 저장.
+                        if (inwgs84Lat) { //inwgs84Lat true일 때 태그의 내용을 저장.
                             wgs84Lat = parser.getText();
                             inwgs84Lat = false;
                         }
-                        if (inwgs84Lon) { //isMapy이 true일 때 태그의 내용을 저장.
+                        if (inwgs84Lon) { //inwgs84Lon true일 때 태그의 내용을 저장.
                             wgs84Lon = parser.getText();
                             inwgs84Lon = false;
                         }
-                        if (inzipcode1) { //isMapy이 true일 때 태그의 내용을 저장.
+                        if (inzipcode1) { //inzipcode1 true일 때 태그의 내용을 저장.
                             zipcode1 = parser.getText();
                             inzipcode1 = false;
                         }
-                        if (inzipcode2) { //isMapy이 true일 때 태그의 내용을 저장.
+                        if (inzipcode2) { //inzipcode2 true일 때 태그의 내용을 저장.
                             zipcode2 = parser.getText();
                             inzipcode2 = false;
                         }
--- FindAED(SEO)/app/src/main/java/kau/smc/findaed/MainActivity.kt
+++ FindAED(SEO)/app/src/main/java/kau/smc/findaed/MainActivity.kt
@@ -1,13 +1,18 @@
 package kau.smc.findaed
 
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import com.google.firebase.database.*
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.*
+import com.naver.maps.map.overlay.InfoWindow
 import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.overlay.Overlay
 import com.naver.maps.map.util.FusedLocationSource
+import org.w3c.dom.Comment
 
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -17,11 +22,18 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setContentView(R.layout.activity_main)
+
+        // Realtime-Database에 message 태그 생성 후 "Hello, World!" 추가
+         var database: FirebaseDatabase = FirebaseDatabase.getInstance()
+         var databaseReference: DatabaseReference = database.getReference("records")
+        // databaseReference.setValue("Hello, World!")
+
+        // 맵 API 호출
         NaverMapSdk.getInstance(this).client =
             NaverMapSdk.NaverCloudPlatformClient("fbjwqr85b8")
 
-        setContentView(R.layout.activity_main)
-
+        // API 호출을 위한 NaverMap 객체 등록
         val fm = supportFragmentManager
         val mapFragment = fm.findFragmentById(R.id.map) as MapFragment?
             ?: MapFragment.newInstance().also {
@@ -30,15 +42,22 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
         mapFragment.getMapAsync(this)
 
+        // '현 위치 관련 기능 (FusedLocationSource)' 사용
         locationSource =
             FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int,
-                                            permissions: Array<String>,
-                                            grantResults: IntArray) {
-        if (locationSource.onRequestPermissionsResult(requestCode, permissions,
-                grantResults)) {
+    // '현 위치 관련 기능 (FusedLocationSource)' 사용
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (locationSource.onRequestPermissionsResult(
+                requestCode, permissions,
+                grantResults
+            )
+        ) {
             return
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
@@ -47,20 +66,63 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(naverMap: NaverMap) {
 
         val uiSettings = naverMap.uiSettings
+        val marker = Marker()
+
+        // 정보창의 내용을 지정한다.
+        val infoWindow = InfoWindow()
+        infoWindow.adapter = object : InfoWindow.DefaultTextAdapter(this) {
+            override fun getText(infoWindow: InfoWindow): CharSequence {
+                return "정보 창 내용"
+            }
+        }
+
+        marker.position = LatLng(37.5921280, 126.97942)
+        marker.map = naverMap
+
+        // 마커의 위치에 정보창을 띄운다.
+        infoWindow.open(marker)
+
+        // 현 위치 버튼 활성화
         uiSettings.isLocationButtonEnabled = true
 
+        // '현 위치 관련 기능 (FusedLocationSource)' 사용
         naverMap.locationSource = locationSource
 
+        // 맵을 클릭했을 때 불린다.
         naverMap.setOnMapClickListener { point, coord ->
-            Toast.makeText(this, "${coord.latitude}, ${coord.longitude}",
-                Toast.LENGTH_SHORT).show()
+            Toast.makeText(
+                this, "${coord.latitude}, ${coord.longitude}",
+                Toast.LENGTH_SHORT
+            ).show()
 
-            val marker = Marker()
+            /*
             marker.position = LatLng(coord.latitude.toDouble(), coord.longitude.toDouble())
             marker.map = naverMap
+            */
+
+            // 맵을 클릭하면 정보창을 닫는다.
+            infoWindow.close()
         }
+
+        // 마커 클릭 시 호출되는 리스너
+        val listener = Overlay.OnClickListener { overlay ->
+            val marker = overlay as Marker
+
+            if (marker.infoWindow == null) {
+                // 현재 마커에 정보 창이 열려있지 않을 경우 엶
+                infoWindow.open(marker)
+            } else {
+                // 이미 현재 마커에 정보 창이 열려있을 경우 닫음
+                infoWindow.close()
+            }
+
+            true
+        }
+
+        marker.onClickListener = listener
     }
 
+    // '현 위치 관련 기능 (FusedLocationSource)' 사용
     companion object {
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
