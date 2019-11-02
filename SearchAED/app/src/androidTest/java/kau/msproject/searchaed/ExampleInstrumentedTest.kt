c9838e1b060f7e8cb23cd23424ba0d9e82b67639
qmdl980 <qmdl98056@gmail.com>
2019-11-02 14:25:02 +0900

    20191102OH

--- SearchAED/app/src/androidTest/java/kau/msproject/searchaed/ExampleInstrumentedTest.kt
+++ SearchAED/app/src/androidTest/java/kau/msproject/searchaed/ExampleInstrumentedTest.kt
@@ -1,7 +1,7 @@
 package kau.msproject.searchaed
 
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
+//import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.runner.AndroidJUnit4
 
 import org.junit.Test
@@ -14,7 +14,7 @@ import org.junit.Assert.*
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
+@RunWith(androidx.test.runner.AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
